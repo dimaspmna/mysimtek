ecc008bd53f90b6cdde4c6f9082e104547a31092
dimaspmna <dimaspmna@gmail.com>
2026-07-06 22:42:50 +0700

    chore: bump to v1.6.0, tambah fitur OTP di changelog

--- lib/core/constants/app_about.dart
+++ lib/core/constants/app_about.dart
@@ -1,5 +1,6 @@
 class AppAbout {
   static const List<String> features = [
+    'Login menggunakan Nomor Whatsapp OTP',
     'Pembayaran tagihan via Midtrans (QRIS, GoPay, Virtual Account Bank, dan Transfer Bank).',
     'Chat Realtime untuk Komplain Layanan Internet.',
     'Mengubah Tampilan aplikasi ke versi Terbaru dan Modern.',
